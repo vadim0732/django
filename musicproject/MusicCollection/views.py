192e746992875255e26c0813202ea005d4beb8a7
Студент группы IS <is@sielom.local>
2025-09-24 11:20:18 +0500

    second_commit

--- musicproject/MusicCollection/views.py
+++ musicproject/MusicCollection/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def hello(request):
+    text = "Lorem ipsum"
+    return render(request, 'main.html', {'my_note': text})
+
+
